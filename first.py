a6b46e27416e355bd1d4da9bce22ce6c635fb923
H2212 <viktorholyak1@gmail.com>
2026-03-08 12:29:23 +0200

    changed a lot, still wip

--- first.py
+++ first.py
@@ -1,2 +1,8 @@
-input("I can guess your age with one simple question")
-print("How old are you?")
+class Student:
+    def __init__(self, height = 160):
+        self.height = height
+        self.width = 151
+
+jon = Student()
+kim = Student(height=149)
+print(f"I am {jon.height} kilometers tall and {jon.width} miles wide. And to remind you, yes, im taller than kim, hes only {kim.height} kilometers tall")
